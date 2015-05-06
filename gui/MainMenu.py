25237f6d4f681c8d9db5bd874825584963205897
Richard <polzin.richard@gmail.com>
2015-05-06 10:30:27 +0200

    Cleaned up the project structure

---
+++ gui/MainMenu.py
@@ -0,0 +1,12 @@
+import kivy
+kivy.require("1.9.0")
+
+from kivy.uix.widget import Widget
+
+class Desktop(Widget):
+    def mainMenuButtonPressed(menuItem):
+        pass
+
+class Mobile(Widget):
+    def mainMenuButtonPressed(menuItem):
+        pass
--- main.py
+++ main.py
@@ -3,13 +3,23 @@ kivy.require("1.9.0")
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-
-class MainMenu(Widget):
-    pass
+from kivy import platform
+from kivy.lang import Builder
+from gui import MainMenu
 
 class ChumPyApp(App):
+    def loadKVfiles(self):
+        Builder.load_file("gui/mainmenu.kv")
+
     def build(self):
-        return MainMenu()
+        self.loadKVfiles()
+        #platform is either win, linux, android, macosx or unknown
+        if platform == 'android':
+            ui = MainMenu.Mobile()
+        else:
+            ui = MainMenu.Desktop()
+        return ui
+
 
 if __name__ == "__main__":
     ChumPyApp().run()
