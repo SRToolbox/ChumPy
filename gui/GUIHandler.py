3a233dc972ee62b167a18d15a845da1bcbb0f83b
Richard <polzin.richard@gmail.com>
2015-05-12 10:08:47 +0200

    Added Button events

--- gui/GUIHandler.py
+++ gui/GUIHandler.py
@@ -1,4 +1,40 @@
+import gettext
+en = gettext.translation("en",localedir="locale",languages=["en"])
+de = gettext.translation("de",localedir="locale",languages=["de"])
+en.install()
+
 class Button:
+
     @staticmethod
     def buttonPressed(*args):
+        origin = args[1]
+        button = args[2]
+        Button.mappings[button]()
         pass
+
+    def CreateButtonPressed():
+        pass
+
+    def MaintainButtonPressed():
+        pass
+
+    def PlayButtonPressed():
+        pass
+
+    def GameMasterButtonPressed():
+        pass
+
+    def ToolsButtonPressed():
+        pass
+
+    def AboutButtonPressed():
+        pass
+
+    mappings = {
+        _("Create"):CreateButtonPressed,
+        _("Maintain"):MaintainButtonPressed,
+        _("Play"):PlayButtonPressed,
+        _("GameMaster"):GameMasterButtonPressed,
+        _("Tools"):ToolsButtonPressed,
+        _("About"):AboutButtonPressed,
+    }
